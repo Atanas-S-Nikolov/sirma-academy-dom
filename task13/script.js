63e8d87b125e7e5214fc580f43ace411bbe752d4
Atanas-S-Nikolov <atanas.stoyanov1199@gmail.com>
2024-07-09 11:54:59 +0300

    Fix bugs

--- task13/script.js
+++ task13/script.js
@@ -1,52 +1,77 @@
+function isNotBlank(str) {
+  return str && !/^\s+$/.test(str);
+}
+
 function handleInputEnterPress(event, originalTextContent) {
-    const key = event.key.toLowerCase();
-    const isKeyEnter = key === "enter";
-    const isKeyEsc = key === "escape";
+  const key = event.key.toLowerCase();
+  const isKeyEnter = key === "enter";
+  const isKeyEsc = key === "escape";
 
-    const input = document.querySelector(".item-edit");
-    
-    if (!isKeyEnter && !isKeyEsc) {
-        return;
-    }
+  const input = document.querySelector(".item-edit");
+  const { value } = input;
 
-    const listItem = document.createElement("li");
-    listItem.title = "Click to edit";
-    listItem.onclick = editItem;
+  if (!isKeyEnter && !isKeyEsc) {
+    return;
+  }
 
-    if (isKeyEnter) {
-        listItem.textContent = input.value;
-    } else {
-        listItem.textContent = originalTextContent;
-    }
+  const listItem = document.createElement("li");
+  listItem.title = "Click to edit";
+  listItem.onclick = editItem;
 
-    const list = document.querySelector('#list-app ul#items');
+  if (isKeyEnter) {
+    listItem.textContent = value;
+  } else {
+    listItem.textContent = originalTextContent;
+  }
+
+  if (isNotBlank(value) || isKeyEsc) {
+    const list = document.querySelector("#list-app ul#items");
     list.replaceChild(listItem, input);
+  }
 }
 
 function editItem(event) {
-    event.preventDefault();
-    
-    const list = document.querySelector('#list-app ul#items');
-    const input = document.createElement("input");
-    const itemTextContent = event.target.textContent;
+  event.preventDefault();
 
-    input.classList.add("item-edit");
-    input.value = itemTextContent;
-    input.title = "Press 'enter' to save or 'esc' to cancel";
-    input.onkeydown = (keyboardEvent) => handleInputEnterPress(keyboardEvent, itemTextContent);
+  const list = document.querySelector("#list-app ul#items");
+  const input = document.createElement("input");
+  const itemTextContent = event.target.textContent;
 
-    list.replaceChild(input, event.target);
+  input.classList.add("item-edit");
+  input.value = itemTextContent;
+  input.title = "Press 'enter' to save or 'esc' to cancel";
+  input.onkeydown = (keyboardEvent) =>
+    handleInputEnterPress(keyboardEvent, itemTextContent);
+
+  list.replaceChild(input, event.target);
 }
 
 function createItem(event) {
-    event.preventDefault();
-    
-    const listItem = document.createElement('li');
-    const inputText = document.querySelector('input[type="text"]')
-    listItem.textContent = inputText.value;
+  event.preventDefault();
+
+  const listItem = document.createElement("li");
+  const inputEl = document.querySelector('input[type="text"]');
+  const { value } = inputEl;
+  if (isNotBlank(value)) {
+    listItem.textContent = value;
     listItem.title = "Click to edit";
     listItem.onclick = editItem;
-
-    const list = document.querySelector('#list-app ul#items');
+    const list = document.querySelector("#list-app ul#items");
     list.appendChild(listItem);
+    inputEl.value = "";
+    document.getElementById("create-btn").disabled = true;
+  }
+}
+
+function handleInputChange(event) {
+  const { value } = event.target;
+  const buttonEl = document.getElementById("create-btn");
+  const isValid = isNotBlank(value);
+  const buttonOnClick = isValid
+    ? createItem
+    : (event) => {
+        event.preventDefault();
+      };
+  buttonEl.disabled = !isValid;
+  buttonEl.onclick = buttonOnClick;
 }
